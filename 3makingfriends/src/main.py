a0c25fc78c529adc3682e01cc69fbfc9d2a8fd60
Alice Wonderland <borglund.g@gmial.com>
2020-03-27 08:33:05 +0100

    Wow

--- 3makingfriends/src/main.py
+++ 3makingfriends/src/main.py
@@ -1,11 +1,12 @@
 import sys
 
 def main():
-    print("Tja")
     file = sys.stdin.read().split()
 
+    n = int(file[0])
+    m = int(file[1])
 
-
+    edges = file[2:]
 
 if __name__ == "__main__":
     main()
