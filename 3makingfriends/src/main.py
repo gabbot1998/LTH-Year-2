cf3f022a1787223f995fb569b03c13e3fad3e00e
Alice Wonderland <borglund.g@gmial.com>
2020-03-27 11:56:36 +0100

    wow :3

--- 3makingfriends/src/main.py
+++ 3makingfriends/src/main.py
@@ -7,8 +7,10 @@ def main():
     m = file[1]
 
     edges = file[2:]
+    total_weight = 0
 
     trees = []
+    added_to_trees = set()
     weight_list = []
 
 
@@ -21,6 +23,13 @@ def main():
         person2 = edges[3*i + 1]
         weight = edges[3*i + 2]
         weight_list.append((person1, person2, weight))
+        if person1 not in added_to_trees:
+            trees.append({person1})
+            added_to_trees.add(person1)
+        if person2 not in added_to_trees:
+            trees.append({person2})
+            added_to_trees.add(person2)
+
 
 
 
@@ -29,18 +38,26 @@ def main():
     for pair in weight_list:
         person1 = pair[0]
         person2 = pair[1]
+        weight = pair[2]
         tree1, i1 = find_tree_containing(trees, person1)
         tree2, i2 = find_tree_containing(trees, person2)
-        if tree1 != tree2:
-            del trees[i1]
-            del trees[i2]
-            trees.append(tree1.union(tree2))
+        #print(tree1)
+        #print(tree2)
 
-    print(trees)
+        if len(trees) != 1:
+            if tree1 != tree2:
+                trees.remove(tree1)
+                trees.remove(tree2)
+                #print("adding: " + str(weight))
+                total_weight = total_weight + weight
+                trees.append(tree1.union(tree2))
+
+    print(total_weight)
 
 
 def find_tree_containing(trees, person):
     i = -1
+    #print(trees)
     for tree in trees:
         i = i + 1
         if person in tree:
