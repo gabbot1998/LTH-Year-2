ce09772e254a03fb1b58e5af5af2569f54193ac7
Alice Wonderland <borglund.g@gmial.com>
2020-05-13 13:55:19 +0200

    wow

--- 5gorilla/src/main.py
+++ 5gorilla/src/main.py
@@ -3,13 +3,29 @@ import math
 from datetime import datetime
 
 def main():
-    n = read_input()
+    scores, queries = read_input()
+    
 
 def read_input():
     file = list(sys.stdin.read().split('\n'))
     chars = file[0].split()
     nbrOfChars = len(chars)
     scoress = file[1:nbrOfChars + 1]
+    nbrQueries = file[nbrOfChars + 1]
+    arr = []
+    newscore = []
+    qrs = file[nbrOfChars + 2:-1]
+    queries = []
+
+    for q in qrs:
+        q1 = q.split()
+        queries.append((q1[0], q1[1]))
+
+    for line in range(len(scoress)):
+        newscore.append(list(map(int,scoress[line].split())))
+
+    for line in scoress:
+        arr.append(line.replace(" ", ""))
 
     i = 0
     j = 0
@@ -17,10 +33,13 @@ def read_input():
     for c1 in chars:
         scores[c1] = {}
         for c2 in chars:
-            scores[c1][c2] = scoress[i][j]
+            scores[c1][c2] = newscore[i][j]
             j += 1
-    i += 1        
-    print(scoress)
+        i += 1
+        j = 0
+
+    return scores, queries
+
 
 if __name__ == "__main__":
     main()
