0718b9089136514729d7f9bed8f8dcdc933e889e
mahirhambiralovic <mahir.hambiralovic@gmail.com>
2020-05-16 17:27:57 +0200

    minor changes

--- 5gorilla/src/main.py
+++ 5gorilla/src/main.py
@@ -1,5 +1,6 @@
 import sys
 import math
+from array import *
 from datetime import datetime
 
 def main():
@@ -19,16 +20,19 @@ def main():
 def opt(query,i,j,scores,A):
     #print()
     print(i,j)
+    for line in A:
+        print(line)
     if A[i][j] != None: # Use what you already know
+        print("found")
         return A[i][j]
 
     # Check if any is 0
     if i == 0:
-        cost = j*scores['space'] # One will be zero ;)
+        cost = j*scores['space']
         A[0][j] = (('*'*j,query[1][:j]),cost)
         return A[0][j]
     if j == 0:
-        cost = i*scores['space'] # One will be zero ;)
+        cost = i*scores['space']
         A[i][0] = ((query[0][:i],'*'*i),cost)
         return A[i][0]
 
@@ -44,7 +48,6 @@ def opt(query,i,j,scores,A):
 
     sp3, score3 = opt(query, (i), (j-1), scores, A)
     c = scores['space'] + score3
-
     biggest = max([a, b, c])
     if a == biggest:
         A[i][j] = ((sp1[0] + query[0][i - 1], sp1[1] + query[1][j - 1]), a)
@@ -55,88 +58,6 @@ def opt(query,i,j,scores,A):
     elif c == biggest:
         A[i][j] = ((sp3[0] + '*', sp3[1] + query[1][j - 1]), c)
         return A[i][j]
-
-
-def printValue(q, A, l_w1, l_w2):
-    # print(q)
-    s1 = ""
-    s2 = ""
-    q1r = q[0][::-1]
-    q2r = q[1][::-1]
-    # x = len(A)-1
-    # y = len(A[0])-1
-    # #Trying the reverse?
-    # while x > 0 or y > 0:
-    #     #print("pre x:{} y:{}".format(x, y))
-    #     use_both = A[x-1][y-1]
-    #     use_first = A[x-1][y]
-    #     use_second = A[x][y-1]
-    #
-    #     best = max(use_both, use_first, use_second)
-    #     if best == use_both:
-    #         s1 = s1 +(q[0][x-1])
-    #         s2 = s2 +(q[1][y-1])
-    #         x-=1
-    #         y-=1
-    #     elif best == use_first:
-    #         s1 = s1 +(q[0][x-1])
-    #         s2 = s2 +('*')
-    #         x-=1
-    #     elif best == use_second:
-    #         s1 = s1 +('*')
-    #         s2 = s2 +(q[1][y-1])
-    #         y-=1
-    #     print(s1[::-1],s2[::-1])
-    # s1 = s1[::-1] #reverse string
-    # s2 = s2[::-1]
-    x=len(q1r)-1
-    y=len(q2r)-1
-    ## FIRST ENTRY
-    # use_both = A[x][y]
-    # use_first = A[x-1][y]
-    # use_second = A[x][y-1]
-    # best = max(use_both, use_first, use_second)
-    # if best == use_both:
-    #     s1 = s1 +(q[0][x-1])
-    #     s2 = s2 +(q[1][y-1])
-    #     x-=1
-    #     y-=1
-    # elif best == use_first:
-    #     s1 = s1 +(q[0][x-1])
-    #     s2 = s2 +('*')
-    #     x-=1
-    # elif best == use_second:
-    #     s1 = s1 +('*')
-    #     s2 = s2 +(q[1][y-1])
-    #     y-=1
-    # print(s1[::-1],s2[::-1])
-    while x > 0 or y > 0:
-        # print("pre x:{} y:{}".format(x, y))
-
-        if x+1 < len(A) and y-1 < len(A[0]): use_both = A[x-1][y-1]
-        if x+1 < len(A): use_first = A[x-1][y]
-        if y+1 < len(A[0]): use_second = A[x][y-1]
-
-        best = max(use_both, use_first, use_second)
-        if best == use_both:
-            s1 = s1 +(q[0][x-1])
-            s2 = s2 +(q[1][y-1])
-            x-=1
-            y-=1
-        elif best == use_first:
-            s1 = s1 +(q[0][x-1])
-            s2 = s2 +('*')
-            x-=1
-        elif best == use_second:
-            s1 = s1 +('*')
-            s2 = s2 +(q[1][y-1])
-            y-=1
-        # print(s1[::-1],s2[::-1])
-        # print(x,y)
-    s1 = s1[::-1] #reverse string
-    s2 = s2[::-1]
-    print(s1 + " " + s2)
-
 
 def read_input():
     file = list(sys.stdin.read().split('\n'))
