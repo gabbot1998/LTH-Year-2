e69518e6682f57bc2d9c238cd7d0cb78b77377ad
mahirhambiralovic <mahir.hambiralovic@gmail.com>
2020-05-18 15:33:27 +0200

    5 is finally working

--- 5gorilla/src/main.py
+++ 5gorilla/src/main.py
@@ -1,9 +1,9 @@
 import sys
 import math
-# import numpy as np
 from datetime import datetime
 import sys
-sys.setrecursionlimit(1500)
+sys.setrecursionlimit(5000)
+
 
 def main():
     scores, queries = read_input()
@@ -11,55 +11,80 @@ def main():
         l_w1 = len(query[0])
         l_w2 = len(query[1])
         longest = max(l_w1, l_w2)
-        A = [['' for col in range(l_w2+1)] for row in range(l_w1+1)] # One extra char for empty string
+        A = [[None for col in range(l_w2+1)] for row in range(l_w1+1)] # One extra char for empty string
+        ##print(l_w1, l_w2)
+        ##print("Query: {} \nA: {}".format(query,A))
+        #print(scores)
+        opt(query,l_w1,l_w2,scores,A)
         #print(A)
-        #A = np.empty([l_w1 +1, l_w2+1], dtype=object) ### replaced list with np array for faster insertion
 
-        sp, cost = opt(query,l_w1,l_w2,scores,A)
-        print(convert(sp[0]) + " " + convert(sp[1]))
+        printValue(scores, query,A, l_w1 - 1, l_w2 - 1)
 
-def convert(s):
-    str1 = ""
-    return(str1.join(s))
+def printValue(score, q, A, l_w1, l_w2):
+    s1 = ""
+    s2 = ""
+    x = l_w1+1
+    y = l_w2+1
+    while x != 0 or y != 0:
+        #print(s1,s2)
+        #print(x,y)
+        use_both = A[x-1][y-1] + score[q[0][x-1]][q[1][y-1]]
+        use_first = A[x-1][y] + score['space']
+        use_second = A[x][y-1] + score['space']
+
+        currentScore = A[x][y]
+        if currentScore == use_both:
+            s1 = (q[0][x-1]) + s1
+            s2 = (q[1][y-1]) + s2
+            x-=1
+            y-=1
+        elif currentScore == use_first:
+            s1 = (q[0][x-1]) + s1
+            s2 = ('*') + s2
+            x-=1
+        elif currentScore == use_second:
+            s1 = ('*') + s1
+            s2 = (q[1][y-1]) + s2
+            y-=1
+    print(s1 + " " + s2)
 
 
 def opt(query,i,j,scores,A):
-    if A[i][j] != '': # Use what you already know
-        # print("found")
+    ##print()
+    #print(i,j,A)
+    if A[i][j] != None: # Use what you already know
+        cw1 = query[0][i - 1]
+        cw2 = query[1][j - 1]
+        ##print("FOUND value for i:{} j:{} cw1:{}, cw2:{}  ".format(i,j,cw1,cw2))
         return A[i][j]
 
     # Check if any is 0
     if i == 0:
-        cost = j*scores['space']
-        A[0][j] = ((['*']*j,list(query[1])[:j]),cost)
-        return A[0][j]
+        cost = j*scores['space'] # One will be zero ;)
+        ##print("ZERO value for i:{} j:{}, cost: {}".format(i,j,cost))
+        A[0][j] = cost
+        return cost
+
     if j == 0:
-        cost = i*scores['space']
-        A[i][0] = ((list(query[0])[:i],['*']*i),cost)
-        return A[i][0]
+        cost = i*scores['space'] # One will be zero ;)
+        ##print("ZERO value for i:{} j:{}, cost: {}".format(i,j,cost))
+        A[i][0] = cost
+        return cost
 
     cw1 = query[0][i - 1]
     cw2 = query[1][j - 1]
 
     # The real stuff
-    sp1, score1 = opt(query, (i-1), (j-1), scores, A)
-    a = scores[cw1][cw2] + score1
+    #print(cw1, cw2)
+    a = scores[cw1][cw2] + opt(query, (i-1), (j-1), scores, A)
+    #print("updating (i{}),(j{}) to {}".format(i,j,(A[i-1][j-1]) + scores[cw1][cw2]))
+    b = scores['space'] + opt(query, (i-1), (j), scores, A)
+    #print("updating (i{}),(j{}) to {}".format(i,j,A[i-1][j]))
+    c = scores['space'] + opt(query, (i), (j-1), scores, A)
+    #print("updating (i{}),(j{}) to {}".format(i,j,A[i][j - 1]))
 
-    sp2, score2 = opt(query, (i-1), (j), scores, A)
-    b = scores['space'] + score2
-
-    sp3, score3 = opt(query, (i), (j-1), scores, A)
-    c = scores['space'] + score3
-    biggest = max([a, b, c])
-    if a == biggest:
-        A[i][j] = ((sp1[0] + list(query[0][i - 1]), sp1[1] + list((query[1][j - 1]))), a)
-        return A[i][j]
-    elif b == biggest:
-        A[i][j] = ((sp2[0] + list((query[0][i - 1])), list(sp2[1]) + ['*']), b)
-        return A[i][j]
-    elif c == biggest:
-        A[i][j] = ((sp3[0] + ['*'], sp3[1] + list((query[1][j - 1]))), c)
-        return A[i][j]
+    A[i][j] = max([a, b, c])
+    return A[i][j]
 
 def read_input():
     file = list(sys.stdin.read().split('\n'))
